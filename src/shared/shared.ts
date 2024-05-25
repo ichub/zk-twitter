ac30eb137b9f25de0f58731a11435bebedbbb290
Ivan Chub <ivansergchub@gmail.com>
2024-05-25 19:34:50 +0200

    better watermark

--- src/shared/shared.ts
+++ src/shared/shared.ts
@@ -1,1 +1,6 @@
-export const WATERMARK = BigInt(12345);
+export const WATERMARK = uuidToBigInt("89f4e976-a4d0-4dab-9800-612026e685af");
+
+function uuidToBigInt(uuid: string): BigInt {
+  const numericUUID = uuid.replace(/-/g, "");
+  return BigInt("0x" + numericUUID);
+}
