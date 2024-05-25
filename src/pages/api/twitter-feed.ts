af48329bd727b8a5a3c43628469d96567d36ea64
Ivan Chub <ivansergchub@gmail.com>
2024-05-25 16:35:17 +0200

    actually working on issuing the twitter feed

--- src/pages/api/twitter-feed.ts
+++ src/pages/api/twitter-feed.ts
@@ -1,12 +1,54 @@
-import type { NextApiRequest, NextApiResponse } from 'next';
+import {
+  ListFeedsRequest,
+  ListFeedsResponseValue,
+  PollFeedResponseValue,
+  CredentialRequest
+} from "@pcd/passport-interface";
+import { PCDPermissionType } from "@pcd/pcd-collection";
+import type { NextApiRequest, NextApiResponse } from "next";
+import NextCors from "nextjs-cors";
 
 type ResponseData = {
   message: string;
 };
 
-export default function handler(
+export default async function handler(
   req: NextApiRequest,
-  res: NextApiResponse<ResponseData>
+  res: NextApiResponse<ListFeedsResponseValue | PollFeedResponseValue>
 ) {
-  res.status(200).json({ message: 'Hello from Next.js!' });
+  await NextCors(req, res, {
+    // Options
+    methods: ["GET", "HEAD", "PUT", "PATCH", "POST", "DELETE"],
+    origin: "*",
+    optionsSuccessStatus: 200 // some legacy browsers (IE11, various SmartTVs) choke on 204
+  });
+
+  if (req.method === "GET") {
+    res.status(200).json(handleListFeed());
+  } else if (req.method === "POST") {
+    res.status(404);
+  }
+}
+
+function handleListFeed(): ListFeedsResponseValue {
+  return {
+    feeds: [
+      {
+        id: "twitter",
+        name: "Twitter",
+        description: "Twitter",
+        permissions: [
+          { folder: "ZK-Twitter", type: PCDPermissionType.AppendToFolder },
+          { folder: "ZK-Twitter", type: PCDPermissionType.AppendToFolder },
+          { folder: "ZK-Twitter", type: PCDPermissionType.AppendToFolder }
+        ],
+        credentialRequest: {
+          signatureType: "sempahore-signature-pcd",
+          pcdType: "email-pcd"
+        }
+      }
+    ],
+    providerName: "zk-twitter",
+    providerUrl: "http://localhost:4001"
+  };
 }
