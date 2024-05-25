94434f82a196cdcab05fc0cbe48600ed045fc8aa
Ivan Chub <ivansergchub@gmail.com>
2024-05-25 16:40:17 +0200

    ok issuing almost works

--- src/pages/api/twitter-feed.ts
+++ src/pages/api/twitter-feed.ts
@@ -1,33 +1,39 @@
 import {
-  ListFeedsRequest,
   ListFeedsResponseValue,
-  PollFeedResponseValue,
-  CredentialRequest
+  PollFeedRequest,
+  PollFeedResponseValue
 } from "@pcd/passport-interface";
-import { PCDPermissionType } from "@pcd/pcd-collection";
+import { PCDActionType, PCDPermissionType } from "@pcd/pcd-collection";
 import type { NextApiRequest, NextApiResponse } from "next";
 import NextCors from "nextjs-cors";
 
-type ResponseData = {
-  message: string;
-};
+export const FOLDER_NAME = "ZK Twitter";
 
 export default async function handler(
   req: NextApiRequest,
   res: NextApiResponse<ListFeedsResponseValue | PollFeedResponseValue>
 ) {
   await NextCors(req, res, {
-    // Options
     methods: ["GET", "HEAD", "PUT", "PATCH", "POST", "DELETE"],
     origin: "*",
-    optionsSuccessStatus: 200 // some legacy browsers (IE11, various SmartTVs) choke on 204
+    optionsSuccessStatus: 200
   });
 
   if (req.method === "GET") {
     res.status(200).json(handleListFeed());
   } else if (req.method === "POST") {
-    res.status(404);
+    res.status(200).json(await handlePollFeed(req.body));
   }
+}
+
+async function handlePollFeed(
+  req: PollFeedRequest
+): Promise<PollFeedResponseValue> {
+  return {
+    actions: [
+      { type: PCDActionType.AppendToFolder, folder: FOLDER_NAME, pcds: [] }
+    ]
+  };
 }
 
 function handleListFeed(): ListFeedsResponseValue {
@@ -38,9 +44,9 @@ function handleListFeed(): ListFeedsResponseValue {
         name: "Twitter",
         description: "Twitter",
         permissions: [
-          { folder: "ZK-Twitter", type: PCDPermissionType.AppendToFolder },
-          { folder: "ZK-Twitter", type: PCDPermissionType.AppendToFolder },
-          { folder: "ZK-Twitter", type: PCDPermissionType.AppendToFolder }
+          { folder: FOLDER_NAME, type: PCDPermissionType.AppendToFolder },
+          { folder: FOLDER_NAME, type: PCDPermissionType.AppendToFolder },
+          { folder: FOLDER_NAME, type: PCDPermissionType.AppendToFolder }
         ],
         credentialRequest: {
           signatureType: "sempahore-signature-pcd",
@@ -48,7 +54,7 @@ function handleListFeed(): ListFeedsResponseValue {
         }
       }
     ],
-    providerName: "zk-twitter",
-    providerUrl: "http://localhost:4001"
+    providerName: FOLDER_NAME,
+    providerUrl: "http://localhost:4001/api/twitter-feed"
   };
 }
